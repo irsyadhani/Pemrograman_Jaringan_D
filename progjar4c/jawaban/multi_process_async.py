d4641383f5ee1608e05823c495b43c4e42a4aca9
Irsyadhani Dwi Shubhi <58015078+irsyadhani@users.noreply.github.com>
2021-07-16 13:39:44 +0700

    Add

---
+++ progjar4c/jawaban/multi_process_async.py
@@ -0,0 +1,35 @@
+from library import download_gambar, get_url_list, kirim_gambar
+import time
+import datetime
+from multiprocessing import Process, Pool
+
+def kirim_server():
+    texec = dict()
+    urls = get_url_list()
+    status_task = dict()
+    temp = 0
+    task_pool = Pool(processes=20)
+    catat_awal = datetime.datetime.now()
+    for k in urls:
+        download_gambar(urls[k],k)
+        print(f"mendownload {urls[k]}")
+        UDP_IP_ADDRESS = "192.168.122.157"
+        UDP_IP_ADDRESS2 = "192.168.122.199"
+        PORT = 5003
+        if temp == 0:
+            texec[k] = task_pool.apply_async(func=kirim_gambar, args=(UDP_IP_ADDRESS,PORT,f"{k}.jpg"))
+            print('Masuk server 1')
+            temp = temp+1
+        elif temp == 1:
+            print('Masuk server 2')
+            texec[k] = task_pool.apply_async(func=kirim_gambar, args=(UDP_IP_ADDRESS2,PORT,f"{k}.jpg"))
+    for k in urls:
+        status_task[k]=texec[k].get(timeout=10)
+
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+    print("status TASK")
+    print(status_task)
+if __name__=='__main__':
+    kirim_server()
